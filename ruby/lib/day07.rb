11c7a5ed3f7651531006490c0541021c44b5918e
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2021-12-07 17:11:01 +0200

    I am out of ideas so I made inconsequential changes

--- ruby/lib/day07.rb
+++ ruby/lib/day07.rb
@@ -7,31 +7,35 @@ module Day7
   LINES = File.readlines(FP).map { |x| x.chomp!("\n") }
 
   class Common
+    attr_reader :horizontal_positions
+
+    def initialize
+      @horizontal_positions = Common.parse_input.flatten
+    end
+
     def self.parse_input
       LINES.map do |line|
         line.split(',').map { |num| Integer(num) }
       end
     end
 
-    def self.compute_consumption(hposes, i)
+    def compute_consumption(i)
       fuels = []
 
-      hposes.each_with_index do |pos, cur|
+      @horizontal_positions.each_with_index do |pos, cur|
         next if cur == i
 
-        fuel = self.compute_cost(pos, hposes[i])
+        fuel = self.compute_cost(pos, @horizontal_positions[i])
         fuels << fuel unless fuel.nil?
       end
 
       fuels.reduce(:+)
     end
 
-    def self.solve
-      hposes = parse_input.flatten
-
+    def solve
       min_fuel = 10000000000
-      hposes.each_with_index do |hpos, i|
-        fuel = compute_consumption(hposes, i)
+      @horizontal_positions.each_with_index do |hpos, i|
+        fuel = compute_consumption(i)
         min_fuel = [min_fuel, fuel].min
       end
 
@@ -40,14 +44,14 @@ module Day7
   end
 
   class Part1 < Common
-    def self.compute_cost(a, b)
+    def compute_cost(a, b)
       (a - b).abs
     end
 
   end
 
   class Part2 < Common
-    def self.compute_cost(a, b)
+    def compute_cost(a, b)
       x, y = [a, b].minmax
       (1..(y - x)).reduce(:+)
     end
--- ruby/spec/day07_spec.rb
+++ ruby/spec/day07_spec.rb
@@ -2,12 +2,12 @@ require_relative '../lib/day07'
 
 RSpec.describe 'Day 7' do
   it 'solves the first part' do
-    expect(Day7::Part1.solve).to eq(351901)
+    expect(Day7::Part1.new.solve).to eq(351901)
   end
 
   it 'solves the second part' do
     skip 'inefficient (but correct!) algorithm' do
-      expect(Day7::Part2.solve).to eq(101079875)
+      expect(Day7::Part2.new.solve).to eq(101079875)
     end
   end
 end
